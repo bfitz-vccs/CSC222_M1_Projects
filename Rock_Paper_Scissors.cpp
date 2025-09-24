c8700d8264340f32750a2410cfe967db4a94bfd6
bfitz-vccs <bf2877@email.vccs.edu>
2025-09-24 00:39:15 -0400

    Edited tasks.json file and switched to g++ due to compiler error.

--- Rock_Paper_Scissors.cpp
+++ Rock_Paper_Scissors.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <random>
+#include <string>
+
+int player_option();
 
 int main(){
-
+    int result = player_option();
+    std::cout << "Result: " << result << std::endl;
     
     return 0;
 }
+
+int player_option(){
+
+    std::string option;
+    std::cout << "Please enter in rock, paper, or scissors (Q to quit): ";
+    std::cin >> option;
+    
+    while (option != "rock" && option != "paper" && option != "scissors" && option != "Q"){
+        std::cout << "Invalid choice, please try again." << std::endl;
+        std::cout << "Please enter in rock, paper, or scissors (Q to quit): ";
+        std::cin >> option;
+    }
+
+    if (option == "rock"){
+        return 1;
+    }
+    else if (option == "paper"){
+        return 2;
+    }
+    else if (option == "scissors"){
+        return 3;
+    }
+    else{
+        return 0;
+    }
+        
+    }
